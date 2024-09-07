860068edcc2e7f620b85d74e8b8edf1fe45f6832
farish-jamal <farishjamal98@gmail.com>
2024-09-07 15:15:14 +0530

    Added Routes and Standard Api Error and Response

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = express();
 require("dotenv").config();
 
 const port = 8000 || process.env.PORT;
+const adminRoute = require("./src/routes/admin.routes");
 
 // Database Connection
 handleConnectionToDB(process.env.MONGO_URI);
+
+app.use('/api/admin', adminRoute);
 
 app.listen(port, () => {
   console.log(`Server Running on ${port}`)
