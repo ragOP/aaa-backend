c94889f7b8b571edd5765ba408523e23aebf6333
farish-jamal <farishjamal98@gmail.com>
2024-11-23 16:45:32 +0530

    Chnages in Get My complaints

--- src/controllers/customer.controllers.js
+++ src/controllers/customer.controllers.js
@@ -9,18 +9,18 @@ const ApiResponse = require("../utils/ApiResponse");
 exports.handleCustomerLogin = asyncHandler(async (req, res) => {
   const { userName, password } = req.body;
 
-  const { customer, token, message } = await customerLogin(userName, password);
+  const { customer, token, message, statusCode } = await customerLogin(userName, password);
   if (!customer) {
-    return res.status(200).json(
-      new ApiResponse(200, {
+    return res.status(statusCode).json(
+      new ApiResponse(statusCode, {
         message,
       })
     );
   }
 
   return res
-    .status(200)
-    .json(new ApiResponse(200, { user: customer, token }, message));
+    .status(statusCode)
+    .json(new ApiResponse(statusCode, { user: customer, token }, message));
 });
 
 exports.handleNewComplaint = asyncHandler(async (req, res) => {
@@ -33,23 +33,22 @@ exports.handleNewComplaint = asyncHandler(async (req, res) => {
       .status(400)
       .json(new ApiResponse(400, { messaage: "Invalid request" }));
   }
-  const { complaint, messaage } = await createComplaint(
+  const { complaint, messaage, statusCode } = await createComplaint(
     customerId,
     complaintData,
     images,
     voiceNote
   );
   if (!complaint) {
-    return res.status(500).json(
-      new ApiResponse(500, {
+    return res.status(statusCode).json(
+      new ApiResponse(statusCode, {
         messaage,
       })
     );
   }
-  console.log(complaint, messaage);
   return res
-    .status(201)
-    .json(new ApiResponse(201, { data: complaint }, messaage));
+    .status(statusCode)
+    .json(new ApiResponse(statusCode, { data: complaint }, messaage));
 });
 
 exports.handleGetMyComplaint = asyncHandler(async (req, res) => {
@@ -59,15 +58,15 @@ exports.handleGetMyComplaint = asyncHandler(async (req, res) => {
      .status(400)
      .json(new ApiResponse(400, { messaage: "Please Provide Customer Id" }));
   }
-  const { complaint, messaage } = await getMyComplaint(customerId);
-  if (!complaint) {
-    return res.status(500).json(
-      new ApiResponse(500, {
+  const { complaints, messaage, statusCode } = await getMyComplaint(customerId);
+  if (!complaints) {
+    return res.status(statusCode).json(
+      new ApiResponse(statusCode, {
         messaage,
       })
     );
   }
   return res
-    .status(200)
-    .json(new ApiResponse(200, { data: complaint }, messaage));
+    .status(statusCode)
+    .json(new ApiResponse(statusCode, { data: complaints }, messaage));
 })
--- src/services/customer.services.js
+++ src/services/customer.services.js
@@ -3,15 +3,16 @@ const bcrypt = require("bcrypt");
 const Customer = require("../models/customer.models");
 const Complaint = require("../models/complaint.models");
 const { uploadMultipleFiles, uploadVoiceNote }  = require("../helper/cloudniary.uploads");
+const Engineer = require("../models/engineer.model");
 
 exports.customerLogin = async (userName, password) => {
   const customer = await Customer.findOne({ userName });
   if (!customer) {
-    return { customer: null, token: null, message: "No user found" };
+    return { customer: null, token: null, message: "No user found", statusCode: 404 };
   }
   const comparePassword = await bcrypt.compare(password, customer.password);
   if (!comparePassword) {
-    return { customer: null, token: null, message: "Password is not correct" };
+    return { customer: null, token: null, message: "Password is not correct", statusCode: 401 };
   }
   const { password: _, ...customerWithoutPassword } = customer.toObject();
   const token = jwt.sign(
@@ -22,18 +23,19 @@ exports.customerLogin = async (userName, password) => {
     customer: customerWithoutPassword,
     token,
     message: "User loggedin successfully",
+    statusCode: 200,
   };
 };
 
 exports.createComplaint = async (customerId, complaintData, images, voiceNote) => {
   if(!images){
-    return { complaint: null, message: "No images provided" };
+    return { complaint: null, message: "No images provided", statusCode: 404 };
   }
   const uploadedUrls = await uploadMultipleFiles(images, 'complaints/images');
   const uploadedAudioUrl = await uploadVoiceNote(voiceNote.path, 'complaints/voice_notes');
   const customer = await Customer.findById(customerId);
   if (!customer) {
-    return { complaint: null, message: "Customer not found" };
+    return { complaint: null, message: "Customer not found", statusCode: 404 };
   }
   const complaint = await Complaint.create({
     customerId,
@@ -41,13 +43,28 @@ exports.createComplaint = async (customerId, complaintData, images, voiceNote) =
     images: uploadedUrls,
     voiceNote: uploadedAudioUrl,
   });
-  return { complaint, messaage: "Comaplaint registered successfully" };
+  return { complaint, messaage: "Comaplaint registered successfully", statusCode: 201 };
 }
 
 exports.getMyComplaint = async (customerId) => {
-  const complaint = await Complaint.find({ customerId });
-  if (!complaint) {
-    return { complaint: null, message: "No complaint found" };
+  const complaints = await Complaint.find({ customerId });
+  if (complaints.length == 0) {
+    return { complaint: null, message: "No complaint found", statusCode: 404 };
   }
-  return { complaint, messaage: "Complaint retrieved successfully" };
+  const populatedComplaints = await Promise.all(
+    complaints.map(async (complaint) => {
+      const customerDetails = await Customer.findById(
+        complaint.customerId
+      ).select("-password");
+      const technicianDetails = await Engineer.findById(
+        complaint.technician
+      ).select("-password");
+      return {
+        ...complaint.toObject(),
+        customerId: customerDetails || null,
+        technician: technicianDetails || null,
+      };
+    })
+  );
+  return { complaints: populatedComplaints, messaage: "Complaint retrieved successfully", statusCode: 200 };
 }
